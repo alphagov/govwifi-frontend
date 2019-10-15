8b0aa381f58a5c17d115a4a3587f7366ddd47032
Anshul Sirur <vixus0@gmail.com>
2019-10-15 16:17:44 +0100

    Make /etc/raddb/certs a volume and add aws-cli container
    We want to run Radius using Fargate and we don't want to bake a load of
    config and certs into the govwifi-frontend image. `Dockerfile.raddb`
    defines an image that will download the certs and client config from S3 and put them in a volume.

---
+++ raddb.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env ash
+
+set -o nounset -o errexit
+
+ENDPOINT_ARG=${ENDPOINT_URL:+--endpoint-url=$ENDPOINT_URL}
+CERTSDIR=${CERTSDIR:-/etc/raddb/certs}
+
+aws ${ENDPOINT_ARG} s3 cp ${WHITELIST_BUCKET}/clients.conf ${CERTSDIR}/clients.conf
+aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/ca.pem ${CERTSDIR}/ca.pem
+aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/comodoCA.pem ${CERTSDIR}/comodoCA.pem
+aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/server.key ${CERTSDIR}/server.key
+aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/server.pem ${CERTSDIR}/server.pem
+
+exec "$@"
