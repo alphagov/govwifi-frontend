06e85936add50a2bbe5f9f58fe522c88b3602b2d
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2022-07-07 12:27:07 +0100

    Rename Whitelist to Allowlist
    In line with the style guide

--- raddb.sh
+++ raddb.sh
@@ -5,7 +5,7 @@ set -o nounset -o errexit
 ENDPOINT_ARG=${ENDPOINT_URL:+--endpoint-url=$ENDPOINT_URL}
 CERTSDIR=${CERTSDIR:-/etc/raddb/certs}
 
-aws ${ENDPOINT_ARG} s3 cp ${WHITELIST_BUCKET}/clients.conf ${CERTSDIR}/clients.conf
+aws ${ENDPOINT_ARG} s3 cp ${ALLOWLIST_BUCKET}/clients.conf ${CERTSDIR}/clients.conf
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/ca.pem ${CERTSDIR}/ca.pem
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/comodoCA.pem ${CERTSDIR}/comodoCA.pem
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/server.key ${CERTSDIR}/server.key
