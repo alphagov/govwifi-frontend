001f7d04006e377ae3a0619c701d5560396a83f0
koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2023-03-30 11:47:56 +0100

    Add tests for EAP-TLS
    So that we have more confidence our EAP-TLS configuration works
    as intended

--- test-app/spec/commands.rb
+++ test-app/spec/commands.rb
@@ -4,7 +4,7 @@ EAP_TTLS_CONFIG_PATH = File.join(ET_CONFIGS_PATH, "eap-ttls.conf.erb")
 
 PEAP_MSCHAPv2_CONFIG_PATH = File.join(ET_CONFIGS_PATH, "peap-mschapv2.conf.erb")
 
-EAP_TLS_CONFIG_PATH = File.join(ET_CONFIGS_PATH, "eap-tls.conf")
+EAP_TLS_CONFIG_PATH = File.join(ET_CONFIGS_PATH, "eap-tls.conf.erb")
 
 SERVICE_STATUS = "radclient -x localhost:18121 status testing123"
 
@@ -13,9 +13,3 @@ PAP_CMD = "radtest testing password localhost 0 testing123"
 CHAP_CMD = "radtest -t chap testing password localhost 0 testing123"
 
 MSCHAP_CMD = "radtest -t mschap testing password localhost 0 testing123"
-
-EAP_TTLS_CMD = "eapol_test -a 127.0.0.1 -c #{EAP_TTLS_CONFIG_PATH} -s testing123"
-
-PEAP_MSCHAPv2_CMD = "eapol_test -a 127.0.0.1 -c #{PEAP_MSCHAPv2_CONFIG_PATH} -s testing123"
-
-EAP_TLS_CMD = "eapol_test -a 127.0.0.1 -c #{EAP_TLS_CONFIG_PATH} -s testing123"
