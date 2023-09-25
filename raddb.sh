31f25f1ebbe1e1b6cd7df15f74263bed57ff2bef
A1Robson <aaron.robson@digital.cabinet-office.gov.uk>
2023-09-25 16:52:42 +0100

    Enable split merged cert

--- raddb.sh
+++ raddb.sh
@@ -4,10 +4,17 @@ set -o nounset -o errexit
 
 ENDPOINT_ARG=${ENDPOINT_URL:+--endpoint-url=$ENDPOINT_URL}
 CERTSDIR=${CERTSDIR:-/etc/raddb/certs}
+
 aws ${ENDPOINT_ARG} s3 cp ${ALLOWLIST_BUCKET}/clients.conf ${CERTSDIR}/clients.conf
-aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/ ${CERTSDIR}/ --recursive --exclude "*" --include "*.pem"
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/server.key ${CERTSDIR}/server.key
 
-/split_pem.py ${CERTSDIR}/ca.pem
+aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/server.pem ${CERTSDIR}/server.pem
+
+aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/ca.pem ${CERTSDIR}/trusted_certificates/ca.pem
+aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/comodoCA.pem ${CERTSDIR}/trusted_certificates/comodoCA.pem
+
+/split_pem.py ${CERTSDIR}/trusted_certificates/ca.pem
+
+rm ${CERTSDIR}/trusted_certificates/ca.pem
 
 exec "$@"
