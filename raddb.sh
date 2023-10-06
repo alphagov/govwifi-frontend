2758ec210c60547bb2de0e2e1464a510badb6e6d
A1Robson <aaron.robson@digital.cabinet-office.gov.uk>
2023-10-06 09:58:25 +0100

    update for aws trusted cert folder

--- raddb.sh
+++ raddb.sh
@@ -13,8 +13,11 @@ aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/server.pem ${CERTSDIR}/server.pem
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/ca.pem ${CERTSDIR}/trusted_certificates/ca.pem
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/comodoCA.pem ${CERTSDIR}/trusted_certificates/comodoCA.pem
 
-/split_pem.py ${CERTSDIR}/trusted_certificates/ca.pem
+aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/trusted_certificates/ ${CERTSDIR}/trusted_certificates/ --recursive --exclude "*" --include "*.pem"
 
+# This bit can be removed once CBA dev completed
+/split_pem.py ${CERTSDIR}/trusted_certificates/ca.pem
 rm ${CERTSDIR}/trusted_certificates/ca.pem
+# ...
 
 exec "$@"
