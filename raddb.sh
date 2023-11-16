208a3a80220203b45b9f49eaa64695e499a461e7
A1Robson <aaron.robson@digital.cabinet-office.gov.uk>
2023-11-16 09:48:25 +0000

    Removed config of comodoca.pem as was not actually required

--- raddb.sh
+++ raddb.sh
@@ -11,7 +11,6 @@ aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/server.key ${CERTSDIR}/server.key
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/server.pem ${CERTSDIR}/server.pem
 
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/ca.pem ${CERTSDIR}/trusted_certificates/ca.pem
-aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/comodoCA.pem ${CERTSDIR}/trusted_certificates/comodoCA.pem
 
 aws ${ENDPOINT_ARG} s3 cp ${CERT_STORE_BUCKET}/trusted_certificates/ ${CERTSDIR}/trusted_certificates/ --recursive --exclude "*" --include "*.pem"
 
